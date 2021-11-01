0e0becc2ec20ef8e609e5615958c795983c285c4
mftth <mfattohi@gmail.com>
2021-11-01 09:49:53 +0100

    work on ML

--- Scripts/Execute_Deconvolution.R
+++ Scripts/Execute_Deconvolution.R
@@ -4,42 +4,51 @@
 ## need decision about if QC should take place or no and if p-value should be calculated or not 
 ## ouput is new SCDC "model", deconvolution result and possibly p-value of deconvolution
 
-Execute_Deconvolution <- function(qc, calc_pval, ...){
+prepare_decon_res <- function(decon_res, clinical_char, p_value){
+  if (p_value){
+    decon_res_prepped <- as.data.frame(cbind(decon_res$decon_res$prop.est.mvw, t(decon_res$decon_res$yeval$spearmany.sample.table), 
+                                             t(decon_res$decon_res$yeval$mADy.sample.table), t(decon_res$decon_res$yeval$RMSDy.sample.table),
+                                             decon_res$p_value_per_sample, clinical_char))
+    colnames(decon_res_prepped) <- c(colnames(Repset_scdc_baron$decon_res$prop.est.mvw), "spearman", "mad", "rmsd", 
+                                     "pearson_pval", "spearman_pval", "mad_pval", "rmsd_pval", "response")
+    decon_res_prepped[,1:(ncol(decon_res$decon_res$prop.est.mvw)+7)] <- sapply(1:(ncol(decon_res$decon_res$prop.est.mvw)+7), 
+                                                                               function(x) as.numeric(decon_res_prepped[,x]))
+    decon_res_prepped$response <- as.factor(decon_res_prepped$response)
+  } else {
+    decon_res_prepped <- as.data.frame(cbind(decon_res$decon_res$prop.est.mvw, t(decon_res$decon_res$yeval$spearmany.sample.table), 
+                                             t(decon_res$decon_res$yeval$mADy.sample.table), t(decon_res$decon_res$yeval$RMSDy.sample.table),
+                                             clinical_char))
+    colnames(decon_res_prepped) <- c(colnames(Repset_scdc_baron$decon_res$prop.est.mvw), "spearman", "mad", "rmsd", "response")
+    decon_res_prepped[,1:(ncol(decon_res$decon_res$prop.est.mvw)+3)] <- sapply(1:(ncol(decon_res$decon_res$prop.est.mvw)+3), 
+                                                                               function(x) as.numeric(decon_res_prepped[,x]))
+    decon_res_prepped$response <- as.factor(decon_res_prepped$response)
+  }
+  
+  return(decon_res_prepped)
+}
+
+
+Execute_Deconvolution <- function(qc, calc_pval, clinical_char, ...){
   if(qc){
     sc_qc <- Quality_Control(...)
     if(calc_pval){
-      decon_res <- Calculate_pvalue(...) # 7 features + cell types
+      decon_res <- Calculate_pvalue(...) 
+      decon_res_prepped <- prepare_decon_res(decon_res = decon_res, clinical_char = clinical_char, p_value = TRUE) 
     } else {
-      decon_res <- Deconvolve_SCDC(...) # 3 features + cell types
+      decon_res <- Deconvolve_SCDC(...) 
+      decon_res_prepped <- prepare_decon_res(decon_res = decon_res, clinical_char = clinical_char, p_value = FALSE)
     }
   } else {
     sc_qc <- NULL
     if(calc_pval){
-      decon_res <- Calculate_pvalue(...) # 7 features + cell types
+      decon_res <- Calculate_pvalue(...) 
+      decon_res_prepped <- prepare_decon_res(decon_res = decon_res, clinical_char = clinical_char, p_value = TRUE)
     } else {
-      decon_res <- Deconvolve_SCDC(...) # 3 features + cell types
+      decon_res <- Deconvolve_SCDC(...) 
+      decon_res_prepped <- prepare_decon_res(decon_res = decon_res, clinical_char = clinical_char, p_value = FALSE)
     }
   }
   
   decon_output <- list("sc_qc" = sc_qc, "decon_res" = decon_res)
   return(decon_output)
-}
-
-prepare_decon_res <- function(decon_res, clinical_char, p_value){
-  ## check if pval calc took place
-  if (p_value){
-    decon_res_prepped <- as.data.frame(cbind(decon_res$decon_res$prop.est.mvw, t(Repset_scdc_baron$decon_res$yeval$spearmany.sample.table), 
-                                             t(Repset_scdc_baron$decon_res$yeval$mADy.sample.table), t(Repset_scdc_baron$decon_res$yeval$RMSDy.sample.table),
-                                             Repset_scdc_baron$p_value_per_sample, repset_meta$Grading))
-  } else {
-    
-  }
-  ## form a dataframe out of the deco output --> maybe do this already in Execute_Deconvolution
-  decon_res <- as.data.frame(cbind(Repset_scdc_baron$decon_res$prop.est.mvw, t(Repset_scdc_baron$decon_res$yeval$spearmany.sample.table), 
-                                   t(Repset_scdc_baron$decon_res$yeval$mADy.sample.table), t(Repset_scdc_baron$decon_res$yeval$RMSDy.sample.table),
-                                   Repset_scdc_baron$p_value_per_sample, repset_meta$Grading))
-  colnames(decon_res) <- c(colnames(Repset_scdc_baron$decon_res$prop.est.mvw), "spearman", "mad", "rmsd",
-                           "pearson_pval", "spearman_pval", "mad_pval", "rmsd_pval", "response") ## p-value
-  decon_res[,1:(ncol(Repset_scdc_baron$decon_res$prop.est.mvw)+7)] <- sapply(1:(ncol(Repset_scdc_baron$decon_res$prop.est.mvw)+7), 
-                                                                             function(x) as.numeric(decon_res[,x]))
 }
--- Scripts/Execute_MachineLearning.R
+++ Scripts/Execute_MachineLearning.R
@@ -12,46 +12,103 @@ library(caret)
 library(skimr)
 library(RANN)
 
-Repset_scdc_baron <- readRDS("~/Masterthesis/Deconvolution_results/Repset_scdc_baron.RDS")
+# Repset_scdc_baron <- readRDS("~/Masterthesis/Deconvolution_results/Repset_scdc_baron.RDS")
+# 
+# repset <- read.table(file = "~/Praktikum/Data/RepSet.S57.tsv", header = TRUE, 
+#                      sep = "\t",)
+# pannen_meta <- read.table(file = "~/Praktikum/Data/Clinial_data_Meta_information.tsv", 
+#                           header = TRUE, sep = "\t")
+# scarpa_meta <- pannen_meta[which(pannen_meta$Study == "Scarpa"),]
+# riemer_meta <- pannen_meta[which(pannen_meta$Study == "Riemer"),]
+# riemer_meta <- riemer_meta[which(riemer_meta$Subtype == "Cancer"),]
+# repset_meta <- rbind(scarpa_meta, riemer_meta)
+# repset_meta$Name <- as.character(repset_meta$Name)
+# repset_meta$Name[1:55] <- paste("X", repset_meta$Name[1:55], sep = "")
+# repset_meta <- repset_meta[match(colnames(repset), repset_meta$Name),]
+# all(repset_meta$Name == colnames(repset))
+# rownames(repset_meta) <- repset_meta$Name
+# all(rownames(repset_meta) == rownames(Repset_scdc_baron$decon_res$prop.est.mvw))
+# 
+# ## form a dataframe out of the deco output --> maybe do this already in Execute_Deconvolution
+# decon_res <- as.data.frame(cbind(Repset_scdc_baron$decon_res$prop.est.mvw, t(Repset_scdc_baron$decon_res$yeval$spearmany.sample.table), 
+#                    t(Repset_scdc_baron$decon_res$yeval$mADy.sample.table), t(Repset_scdc_baron$decon_res$yeval$RMSDy.sample.table),
+#                    Repset_scdc_baron$p_value_per_sample, repset_meta$Grading))
+# colnames(decon_res) <- c(colnames(Repset_scdc_baron$decon_res$prop.est.mvw), "spearman", "mad", "rmsd",
+#                          "pearson_pval", "spearman_pval", "mad_pval", "rmsd_pval", "response") ## p-value
+# decon_res[,1:(ncol(Repset_scdc_baron$decon_res$prop.est.mvw)+7)] <- sapply(1:(ncol(Repset_scdc_baron$decon_res$prop.est.mvw)+7), 
+#                                                                            function(x) as.numeric(decon_res[,x]))
+# str(decon_res)
 
-repset <- read.table(file = "~/Praktikum/Data/RepSet.S57.tsv", header = TRUE, 
-                     sep = "\t",)
-pannen_meta <- read.table(file = "~/Praktikum/Data/Clinial_data_Meta_information.tsv", 
-                          header = TRUE, sep = "\t")
-scarpa_meta <- pannen_meta[which(pannen_meta$Study == "Scarpa"),]
-riemer_meta <- pannen_meta[which(pannen_meta$Study == "Riemer"),]
-riemer_meta <- riemer_meta[which(riemer_meta$Subtype == "Cancer"),]
-repset_meta <- rbind(scarpa_meta, riemer_meta)
-repset_meta$Name <- as.character(repset_meta$Name)
-repset_meta$Name[1:55] <- paste("X", repset_meta$Name[1:55], sep = "")
-repset_meta <- repset_meta[match(colnames(repset), repset_meta$Name),]
-all(repset_meta$Name == colnames(repset))
-rownames(repset_meta) <- repset_meta$Name
-all(rownames(repset_meta) == rownames(Repset_scdc_baron$decon_res$prop.est.mvw))
 
-## form a dataframe out of the deco output --> maybe do this already in Execute_Deconvolution
-decon_res <- as.data.frame(cbind(Repset_scdc_baron$decon_res$prop.est.mvw, t(Repset_scdc_baron$decon_res$yeval$spearmany.sample.table), 
-                   t(Repset_scdc_baron$decon_res$yeval$mADy.sample.table), t(Repset_scdc_baron$decon_res$yeval$RMSDy.sample.table),
-                   Repset_scdc_baron$p_value_per_sample, repset_meta$Grading))
-colnames(decon_res) <- c(colnames(Repset_scdc_baron$decon_res$prop.est.mvw), "spearman", "mad", "rmsd",
-                         "pearson_pval", "spearman_pval", "mad_pval", "rmsd_pval", "response") ## p-value
-decon_res[,1:(ncol(Repset_scdc_baron$decon_res$prop.est.mvw)+7)] <- sapply(1:(ncol(Repset_scdc_baron$decon_res$prop.est.mvw)+7), 
-                                                                           function(x) as.numeric(decon_res[,x]))
-str(decon_res)
-
-
-## descriptive statistics (necessary? kinda)
-skimmed <- skim(decon_res)
-
-## impute missing values if there are any
-if (any(skimmed$n_missing > 0)){
-  preProcess_missingdata_model <- preProcess(decon_res, method = "knnImpute")
-  decon_res <- predict(preProcess_missingdata_model, newdata = decon_res)
+train_ML_model <- function(trainData){
+  ## descriptive statistics (necessary? kinda)
+  skimmed <- skim(trainData)
+  
+  ## impute missing values if there are any
+  if (any(skimmed$n_missing > 0)){
+    preProcess_missingdata_model <- preProcess(trainData, method = "knnImpute")
+    trainData <- predict(preProcess_missingdata_model, newdata = trainData)
+  }
+  
+  ## preprocess data --> which method?
+  preProcess_range_model <- preProcess(trainData, method = "scale")
+  trainData <- predict(preProcess_range_model, newdata = decon_res)
+  
+  
+  ## make repeatedcv on train data and predict on hold out dataset
+  
+  
+  ## feature selection
+  subsets <- c(1:8)
+  ctrl <- rfeControl(functions = rfFuncs,
+                     method = "repeatedcv",
+                     repeats = 5,
+                     verbose = FALSE)
+  
+  rfProfile <- rfe(x = trainData[,-ncol(trainData)], 
+                   y = trainData$response,
+                   sizes = subsets,
+                   rfeControl = ctrl)
+  #rfProfile
+  #predictors(rfProfile)
+  
+  ## train RF model and predict on training data itself
+  fitControl <- trainControl(
+    method = "cv",
+    number = 10,
+    sampling = "down",
+    savePred=T
+  )
+  
+  model_rf <- train(x = trainData[, - ncol(trainData)], 
+                    y = trainData$response, 
+                    method = 'rf', 
+                    metric = "Accuracy",
+                    trControl = fitControl, 
+                    type = "Classification",
+                    ntree = 500)
+  fitted <- predict(model_rf)
+  varimp_rf <- varImp(model_rf)
+  plot(varimp_rf)
+  
+  
+  ## train on selected features
+  trainData_sel <- trainData[, predictors(rfProfile)]
+  model_rf_sel <- train(x = trainData_sel, 
+                        y = trainData$response, 
+                        method = 'rf', 
+                        metric = "Accuracy",
+                        trControl = fitControl, 
+                        type = "Classification",
+                        ntree = 500)
+  fitted_sel <- predict(model_rf_sel)
+  varimp_rf_sel <- varImp(model_rf_sel)
+  plot(varimp_rf_sel)
+  
 }
 
-## preprocess data --> which method?
-preProcess_range_model <- preProcess(decon_res, method = "range")
-decon_res <- predict(preProcess_range_model, newdata = decon_res)
+
+
 
 ## split into training and test set (make it optional?)
 set.seed(4)
@@ -76,37 +133,7 @@ featurePlot(x = trainData[, - ncol(trainData)],
             scales = list(x = list(relation="free"), 
                           y = list(relation="free")))
 
-## feature selection
-subsets <- c(1:8)
-ctrl <- rfeControl(functions = rfFuncs,
-                   method = "cv",
-                   repeats = 5,
-                   verbose = FALSE)
 
-rfProfile <- rfe(x, factor(y),
-                 sizes = subsets,
-                 rfeControl = ctrl)
-rfProfile
-predictors(rfProfile)
-
-## train RF model and predict on training data itself
-fitControl <- trainControl(
-  method = "cv",
-  number = 10,
-  sampling = "down",
-  savePred=T
-)
-
-model_rf <- train(x = trainData[, - ncol(trainData)], 
-                  y = trainData$response, 
-                  method = 'rf', 
-                  metric = "Accuracy",
-                  trControl = fitControl, 
-                  type = "Classification",
-                  ntree = 500)
-fitted <- predict(model_rf)
-varimp_rf <- varImp(model_rf)
-plot(varimp_rf)
 
 ## predict on test data
 predicted <- predict(model_rf, testData)
@@ -126,18 +153,6 @@ models <- caretList(response~ ., data=trainData, trControl=trainControl, methodL
 results <- resamples(models)
 summary(results)
 
-## train on selected features
-trainData_sel <- trainData[, predictors(rfProfile)]
-model_rf_sel <- train(x = trainData_sel, 
-                  y = trainData$response, 
-                  method = 'rf', 
-                  metric = "Accuracy",
-                  trControl = fitControl, 
-                  type = "Classification",
-                  ntree = 500)
-fitted_sel <- predict(model_rf_sel)
-varimp_rf_sel <- varImp(model_rf_sel)
-plot(varimp_rf_sel)
 
 ## predict on test data
 predicted_sel <- predict(model_rf_sel, testData)
@@ -146,3 +161,4 @@ con_mat_sel <- confusionMatrix(reference = factor(testData$response), data = pre
                                mode = "everything", positive = "G3")
 con_mat_test_sel <- confusionMatrix(reference = factor(trainData$response), data = predicted_test_sel, 
                                     mode = "everything", positive = "G3")
+
