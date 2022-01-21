3c408977d8b2a4c094b1291d46412b07f853a82a
mftth <mfattohi@gmail.com>
2022-01-21 15:50:43 +0100

    add changes to numerical analysis and correlation analysis

--- Scripts/correlation_analysis.R
+++ Scripts/correlation_analysis.R
@@ -3,29 +3,33 @@
 ## test if predicted cell type proportions are correlated with clinically relevant characteristics;
 ## also: visualization using heatmaps and box plots (boxplots for p-values)
 
-#factor_to_numeric <-
-
 
 correlation_analysis <- function(decon_output, cell_types = NULL, clinical_characteristic){
-  # clinical characteristic has to be of type either numeric or factor
-  
-  if(!(is.factor(clinical_characteristic) || is.numeric(clinical_characteristic))){
-    stop("clinical_characteristic has to be of type numeric or factor")
-  } else if(is.factor(clinical_characteristic)){
-    ## need to find out how to compute correlation if clinical characteristic is categorical (i.e. factor)
-  }
-  
-  if(!is.data.frame(clinical_characteristics)){
-    #clinical_characteristics <- as.data.frame(clinical_characteristics)
-    stop("clinical_characteristics has to be of type data.frame")
-  }
-  
   ct_prop <- decon_output$decon_res$prop.est.mvw
   if(!is.null(cell_types)){
     ct_prop <- as.data.frame(ct_prop[,cell_types])
     colnames(ct_prop) <- cell_types
   }
   
-  #return the correlations, the p-values and the two plots
+  # clinical characteristic has to be of type either numeric or factor
+  if(!(is.factor(clinical_characteristic) || is.numeric(clinical_characteristic))){
+    stop("clinical_characteristic has to be of type numeric or factor")
+    
+  } else if(is.factor(clinical_characteristic)){
+    # for each ct proportion one anova
+    aov_data <- cbind(ct_prop, clinical_characteristic)
+    aov(as.formula(paste0("clinical_characteristic ~ ", paste0(colnames(ct_prop), collapse = " + "), collape = "")),
+        data = aov_data)
+    
+  } else if(is.numeric(clinical_characteristic)){
+      cor_ct_prop <- as.numeric(cor(ct_prop, clinical_characteristic))
+      cor_ct_prop_pval <- sapply(1:ncol(ct_prop), function(x) cor.test(ct_prop[,x],
+                                                                       clinical_characteristic)$p.value)
+      names(cor_ct_prop) <- colnames(ct_prop)
+      names(cor_ct_prop_pval) <- colnames(ct_prop)
+      return(list("corr" = cor_ct_prop,
+                  "p-values" = cor_ct_prop_pval))
+  }
   
+  #return the correlations, the p-values and the two plots
 }
--- Scripts/numerical_assessment.R
+++ Scripts/numerical_assessment.R
@@ -7,23 +7,38 @@ library(bseqsc)
 
 #set.seed(4)
 
-add_noise <- function(matr, times, res_list = NULL){
-  if(is.null(res_list)){
-    res_list <- list()
-    #vecs <- 1:times
+# add_noise <- function(matr, times, res_list = NULL){
+#   if(is.null(res_list)){
+#     res_list <- list()
+#     #vecs <- 1:times
+#   }
+#   if(times == 0){
+#     #res_list[[times]] <- matr
+#     return(res_list)
+#   } else {
+#     #res_list[[times]] <- add.Gaussian.noise(matr, symm = FALSE, stddev = times*10, mean = times*10+1)
+#     #res_list[[times]] <- jitter(matr, factor = times*10, amount = times*10+1)
+#     #res_list[[length(vecs)-times+1]] <- matr+1
+#     #res_list[[times]] <- matr+1
+#     res_list[[times]] <- matr + matrix(runif(nrow(matr)*ncol(matr), min = 5, max = 500), # rnorm doesnt make enough noise
+#                                        nrow=nrow(matr))
+#     return(add_noise(res_list[[times]], times-1, res_list))
+#   }
+# }
+
+## add noise differently via for loop
+add_noise_iteratively <- function(matr, times){
+  uniform_noise <- lapply(1:times, function(x) matrix(runif(nrow(matr)*ncol(matr), 
+                                                            min = 5, max = 500),nrow=nrow(matr)))
+  added_noise <- list()
+  for (i in 1:times) {
+    if(i == 1){
+      added_noise[[i]] <- matr + uniform_noise[[i]]
+    } else {
+      added_noise[[i]] <- added_noise[[i-1]] + uniform_noise[[i]] 
+    }
   }
-  if(times == 0){
-    #res_list[[times]] <- matr
-    return(res_list)
-  } else {
-    #res_list[[times]] <- add.Gaussian.noise(matr, symm = FALSE, stddev = times*10, mean = times*10+1)
-    #res_list[[times]] <- jitter(matr, factor = times*10, amount = times*10+1)
-    #res_list[[length(vecs)-times+1]] <- matr+1
-    #res_list[[times]] <- matr+1
-    res_list[[times]] <- matr + matrix(runif(nrow(matr)*ncol(matr), min = 5, max = 500), # rnorm doesnt make enough noise
-                                       nrow=nrow(matr))
-    return(add_noise(res_list[[times]], times-1, res_list))
-  }
+  return(added_noise)
 }
 
 ## real bulk RNA-seq data and meta data
@@ -49,7 +64,11 @@ pseudo_bulk_data <- pseudo_bulk$pseudo_eset@assayData$exprs
 #for (i in 1:length(pseudo_bulk_data_noise)) {
 #  colnames(pseudo_bulk_data_noise[[i]]) <- colnames(pseudo_bulk_data)
 #}
+
 pseudo_bulk_data_noise <- add_noise(matr = pseudo_bulk_data, times = 10)
+pseudo_bulk_data_noise2 <- add_noise_iteratively(matr = pseudo_bulk_data, times = 10)
+
+
 
 ## for each matrix (i.e. original and noised ones) perform decon with framework
 pseudo_decon <- Calculate_pvalue(ncores = 15, silent = FALSE, bulk_data = pseudo_bulk_data,
@@ -161,7 +180,7 @@ rmsd_comparison$rmsd[1:length(error_cibersort)] <-
 rmsd_comparison$rmsd[(length(error_cibersort)+1):nrow(rmsd_comparison)] <- 
   sapply(rmse_cibersort[error_cibersort], function(x) mean(x))
 
-rmsd_comparison_plot <- ggplot(rmsd_comparison[1:7,],
+rmsd_comparison_plot <- ggplot(rmsd_comparison,
                                aes(x = noise_iteration, y = rmsd, color = method)) + 
   geom_point() # differences in each method are not visible anymore, plot them individually
 rmsd_comparison_plot
