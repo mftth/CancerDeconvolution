7ec89f61d9935059143445066982abfd01c2d02e
mftth <mfattohi@gmail.com>
2022-01-24 13:46:43 +0100

    finish correlation analysis

--- Scripts/correlation_analysis.R
+++ Scripts/correlation_analysis.R
@@ -1,8 +1,6 @@
 ## Mastherthesis, Melanie Fattohi
 ## correlation analysis 
 ## test if predicted cell type proportions are correlated with clinically relevant characteristics;
-## also: visualization using heatmaps and box plots (boxplots for p-values)
-
 
 correlation_analysis <- function(decon_output, cell_types = NULL, clinical_characteristic){
   ct_prop <- decon_output$decon_res$prop.est.mvw
@@ -12,24 +10,28 @@ correlation_analysis <- function(decon_output, cell_types = NULL, clinical_chara
   }
   
   # clinical characteristic has to be of type either numeric or factor
-  if(!(is.factor(clinical_characteristic) || is.numeric(clinical_characteristic))){
-    stop("clinical_characteristic has to be of type numeric or factor")
+  if(!(is.character(clinical_characteristic) || is.numeric(clinical_characteristic))){
+    stop("clinical_characteristic has to be of type numeric or character")
     
-  } else if(is.factor(clinical_characteristic)){
-    # for each ct proportion one anova
-    aov_data <- cbind(ct_prop, clinical_characteristic)
-    aov(as.formula(paste0("clinical_characteristic ~ ", paste0(colnames(ct_prop), collapse = " + "), collape = "")),
-        data = aov_data)
-    
+  } else if(is.character(clinical_characteristic)){
+    aov_data <- as.data.frame(cbind(ct_prop, clinical_characteristic))
+    aov_call <- lapply(1:(ncol(aov_data)-1), 
+                       function(x) aov(as.formula(paste0(colnames(aov_data)[x], 
+                                                         " ~ clinical_characteristic", collapse = "")),
+                                       data = aov_data))
+    names(aov_call) <- colnames(aov_data)[1:(ncol(aov_data)-1)]
+    aov_summary <- lapply(aov_call, function(x) summary(x))
+    aov_pvalue <- sapply(aov_summary, function(x) x[[1]]$`Pr(>F)`[1])
+    return(list("aov_pvalue" = aov_pvalue))
+      
   } else if(is.numeric(clinical_characteristic)){
-      cor_ct_prop <- as.numeric(cor(ct_prop, clinical_characteristic))
-      cor_ct_prop_pval <- sapply(1:ncol(ct_prop), function(x) cor.test(ct_prop[,x],
-                                                                       clinical_characteristic)$p.value)
-      names(cor_ct_prop) <- colnames(ct_prop)
-      names(cor_ct_prop_pval) <- colnames(ct_prop)
-      return(list("corr" = cor_ct_prop,
-                  "p-values" = cor_ct_prop_pval))
+    cor_ct_prop <- as.numeric(cor(ct_prop, clinical_characteristic))
+    cor_ct_prop_pval <- sapply(1:ncol(ct_prop), function(x) cor.test(ct_prop[,x],
+                                                                     clinical_characteristic)$p.value)
+    names(cor_ct_prop) <- colnames(ct_prop)
+    names(cor_ct_prop_pval) <- colnames(ct_prop)
+    return(list("corr" = cor_ct_prop,
+                "corr_pvalue" = cor_ct_prop_pval))
   }
   
-  #return the correlations, the p-values and the two plots
 }
