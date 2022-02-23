37947fcf0ecaa5c0b237c2ee43438352a0c1fd5c
mftth <mfattohi@gmail.com>
2022-02-23 18:50:40 +0100

    finalize numerical assessment

--- Experiments/numerical_assessment.R
+++ Experiments/numerical_assessment.R
@@ -29,6 +29,9 @@ source("~/Masterthesis/CancerDeconvolution/Scripts/Permute_basis.R")
 add_noise_iteratively <- function(matr, times){
   uniform_noise <- lapply(1:times, function(x) matrix(runif(nrow(matr)*ncol(matr), 
                                                             min = 5, max = 500),nrow=nrow(matr)))
+  #uniform_noise <- lapply(1:times, function(x) matrix(rnorm(nrow(matr)*ncol(matr), 
+  #                                                         mean = 4, sd = 8),nrow=nrow(matr)))
+  
   added_noise <- list()
   for (i in 1:times) {
     if(i == 1){
@@ -57,16 +60,9 @@ pseudo_bulk <- generateBulk_allcells(qc_segerstolpe$sc.eset.qc, ct.varname = "cl
 pseudo_bulk_truep <- pseudo_bulk$truep
 pseudo_bulk_data <- pseudo_bulk$pseudo_eset@assayData$exprs
 
-## add noise to pseudo bulk RNA-seq
-#pseudo_bulk_data_noise <- lapply(seq(0, 100, 10), function(x) sapply(1:ncol(pseudo_bulk_data), 
-#                                                          function(y) jitter(pseudo_bulk_data[,y], factor = y)))
-#for (i in 1:length(pseudo_bulk_data_noise)) {
-#  colnames(pseudo_bulk_data_noise[[i]]) <- colnames(pseudo_bulk_data)
-#}
 
 #pseudo_bulk_data_noise <- add_noise(matr = pseudo_bulk_data, times = 10)
 pseudo_bulk_data_noise <- add_noise_iteratively(matr = pseudo_bulk_data, times = 10)
-
 
 
 ## for each matrix (i.e. original and noised ones) perform decon with framework
@@ -81,17 +77,17 @@ pseudo_decon_noise <- lapply(pseudo_bulk_data_noise,
                                                           bulk_meta = pseudo_bulk$pseudo_eset@phenoData@data,
                                                           sc_data = qc_baron$sc.eset.qc, cell_types = cts,
                                                           ensemble = FALSE, multiple_donors = TRUE))
-pseudo_decon_all <- list(pseudo_decon)
+pseudo_decon_all <- list("original" = pseudo_decon)
 pseudo_decon_all <- c(pseudo_decon_all, pseudo_decon_noise)
 
-ct_props <- lapply(pseudo_decon_all, function(x) x$decon_res$prop.est.mvw)
-pearson_pval <- lapply(pseudo_decon_all, function(x) x$p_value_wy_pearson)
-spearman_pval <- lapply(pseudo_decon_all, function(x) x$p_value_wy_spearman)
-mad_pval <- lapply(pseudo_decon_all, function(x) x$p_value_wy_mad)
-rmsd_pval <- lapply(pseudo_decon_all, function(x) x$p_value_wy_rmsd)
-scdc_metrics <- lapply(ct_props, 
-                       function(x) SCDC_peval(ptrue = pseudo_bulk_truep, pest = x, 
-                                              pest.names = "pseudo_bulk")$evals.table)
+#ct_props <- lapply(pseudo_decon_all, function(x) x$decon_res$prop.est.mvw)
+#pearson_pval <- lapply(pseudo_decon_all, function(x) x$p_value_wy_pearson)
+#spearman_pval <- lapply(pseudo_decon_all, function(x) x$p_value_wy_spearman)
+#mad_pval <- lapply(pseudo_decon_all, function(x) x$p_value_wy_mad)
+#rmsd_pval <- lapply(pseudo_decon_all, function(x) x$p_value_wy_rmsd)
+#scdc_metrics <- lapply(ct_props, 
+#                       function(x) SCDC_peval(ptrue = pseudo_bulk_truep, pest = x, 
+#                                              pest.names = "pseudo_bulk")$evals.table)
 pearson_scdc <- sapply(pseudo_decon_all, function(x) x$statistics_observed$pearson_vec)
 rmsd_scdc <- sapply(pseudo_decon_all, function(x) x$statistics_observed$rmsd_vec)
 
@@ -108,30 +104,48 @@ rmsd_scdc <- sapply(pseudo_decon_all, function(x) x$statistics_observed$rmsd_vec
 spearman_pval_all <- as.data.frame(sapply(pseudo_decon_all, function(x) x$p_value_per_sample$Spearman))
 spearman_pval_all$sample <- colnames(pseudo_bulk_data)
 spearman_pval_all <- melt(spearman_pval_all)
+spearman_pval_all$value <- rev(spearman_pval_all$value)
 spearman_pval_all_plot <- ggplot(spearman_pval_all, aes(x=variable, y=value)) + 
-  geom_boxplot()
+  geom_boxplot() + ylab("SCDC Spearman p-value") +  xlab("noise iteration")
+
+pearson_pval_all <- as.data.frame(sapply(pseudo_decon_all, function(x) x$p_value_per_sample$Pearson))
+pearson_pval_all$sample <- colnames(pseudo_bulk_data)
+pearson_pval_all <- melt(pearson_pval_all)
+#pearson_pval_all$value <- rev(pearson_pval_all$value)
+pearson_pval_all_plot <- ggplot(pearson_pval_all, aes(x=variable, y=value)) + 
+  geom_boxplot() + ylab("SCDC Pearson p-value") +  xlab("noise iteration")
 
 mad_pval_all <- as.data.frame(sapply(pseudo_decon_all, function(x) x$p_value_per_sample$mAD))
 mad_pval_all$sample <- colnames(pseudo_bulk_data)
 mad_pval_all <- melt(mad_pval_all)
-mad_pval_all <- ggplot(mad_pval_all, aes(x=variable, y=value)) + 
-  geom_boxplot()
+mad_pval_all$value <- rev(mad_pval_all$value)
+mad_pval_all_plot <- ggplot(mad_pval_all, aes(x=variable, y=value)) + 
+  geom_boxplot() + ylab("SCDC mAD p-value") +  xlab("noise iteration")
+
+rmsd_pval_all <- as.data.frame(sapply(pseudo_decon_all, function(x) x$p_value_per_sample$RMSD))
+rmsd_pval_all$sample <- colnames(pseudo_bulk_data)
+rmsd_pval_all <- melt(rmsd_pval_all)
+#rmsd_pval_all$value <- rev(rmsd_pval_all$value)
+rmsd_pval_all_plot <- ggplot(rmsd_pval_all, aes(x=variable, y=value)) + 
+  geom_boxplot() + ylab("SCDC RMSD p-value") +  xlab("noise iteration")
 
 pearson_scdc <- as.data.frame(pearson_scdc)
 pearson_scdc$sample <- colnames(pseudo_bulk_data)
 pearson_scdc <- melt(pearson_scdc)
+pearson_scdc$value <- rev(pearson_scdc$value)
 pearson_scdc_plot <- ggplot(pearson_scdc, aes(x=variable, y=value)) + 
-  geom_boxplot()
+  geom_boxplot() + ylab("SCDC Pearson correlation") +  xlab("noise iteration")
 
 rmsd_scdc <- as.data.frame(rmsd_scdc)
 rmsd_scdc$sample <- colnames(pseudo_bulk_data)
 rmsd_scdc <- melt(rmsd_scdc)
+rmsd_scdc$value <- rev(rmsd_scdc$value)
 rmsd_scdc_plot <- ggplot(rmsd_scdc, aes(x=variable, y=value)) + 
-  geom_boxplot()
+  geom_boxplot() + ylab("SCDC RMSD") +  xlab("noise iteration")
 
 
 #save.image("~/Masterthesis/Workspaces/numerical_assessment.RData")
-save.image("~/Masterthesis/test2.RData")
+#save.image("~/Masterthesis/test2.RData")
 
 ## for each matrix (i.e. original and noised ones) perform decon with cibersort/bseqsc
 library(bseqsc)
@@ -148,14 +162,14 @@ pseudo_decon_noise_cibersort <- mclapply(pseudo_bulk_data_noise,
                                                                             deconvolution_algorithm = "bseqsc",
                                                                             models = "Alpha_Beta_Gamma_Delta_Acinar_Ductal_Baron"),
                                         mc.cores = 3, mc.silent = FALSE)
-pseudo_decon_all_cibersort <- list(pseudo_decon_cibersort)
+pseudo_decon_all_cibersort <- list("original" = pseudo_decon_cibersort)
 pseudo_decon_all_cibersort <- c(pseudo_decon_all_cibersort, pseudo_decon_noise_cibersort)
 
 error_cibersort <- which(sapply(pseudo_decon_all_cibersort, 
                                 function(x) class(x))!="try-error")
-ct_props_cibersort <- pseudo_decon_all_cibersort
-ct_props_cibersort[error_cibersort] <- lapply(pseudo_decon_all_cibersort[error_cibersort], 
-                                              function(x) x[,1:(ncol(x)-3)])
+#ct_props_cibersort <- pseudo_decon_all_cibersort
+#ct_props_cibersort[error_cibersort] <- lapply(pseudo_decon_all_cibersort[error_cibersort], 
+#                                              function(x) x[,1:(ncol(x)-3)])
 pearson_pval_cibersort <- pseudo_decon_all_cibersort
 pearson_pval_cibersort[error_cibersort] <- lapply(pseudo_decon_all_cibersort[error_cibersort], 
                                           function(x) x$P_value)
@@ -165,12 +179,12 @@ rmse_cibersort[error_cibersort] <- lapply(pseudo_decon_all_cibersort[error_ciber
 correlation_cibersort <- pseudo_decon_all_cibersort
 correlation_cibersort[error_cibersort] <- lapply(pseudo_decon_all_cibersort[error_cibersort], 
                                                  function(x) x$Correlation)
-scdc_metrics_cibersort <- pseudo_decon_all_cibersort
-ct_props_cibersort[error_cibersort] <- lapply(ct_props_cibersort[error_cibersort], 
-                                              function(x) as.matrix(x))
-scdc_metrics_cibersort[error_cibersort] <- lapply(ct_props_cibersort[error_cibersort], 
-                                                  function(x) SCDC_peval(ptrue = pseudo_bulk_truep, pest = x, 
-                                              pest.names = "pseudo_bulk")$evals.table)
+#scdc_metrics_cibersort <- pseudo_decon_all_cibersort
+#ct_props_cibersort[error_cibersort] <- lapply(ct_props_cibersort[error_cibersort], 
+#                                              function(x) as.matrix(x))
+#scdc_metrics_cibersort[error_cibersort] <- lapply(ct_props_cibersort[error_cibersort], 
+#                                                  function(x) SCDC_peval(ptrue = pseudo_bulk_truep, pest = x, 
+#                                              pest.names = "pseudo_bulk")$evals.table)
 
 
 # plot(sapply(pearson_pval_cibersort[error_cibersort], function(x) mean(x)))
@@ -184,22 +198,22 @@ cibersort_pearson_pval_all <- as.data.frame(do.call(cbind, pearson_pval_cibersor
 cibersort_pearson_pval_all$sample <- colnames(pseudo_bulk_data)
 cibersort_pearson_pval_all <- melt(cibersort_pearson_pval_all)
 cibersort_pearson_pval_all_plot <- ggplot(cibersort_pearson_pval_all, aes(x=variable, y=value)) + 
-  geom_boxplot()
+  geom_boxplot() + ylab("CIBERSORT Pearson p-value") +  xlab("noise iteration")
 
 cibersort_rmse_all <- as.data.frame(do.call(cbind, rmse_cibersort))
 cibersort_rmse_all$sample <- colnames(pseudo_bulk_data)
 cibersort_rmse_all <- melt(cibersort_rmse_all)
 cibersort_rmse_all_plot <- ggplot(cibersort_rmse_all, aes(x=variable, y=value)) + 
-  geom_boxplot()
+  geom_boxplot() + ylab("CIBERSORT RMSE") +  xlab("noise iteration")
 
 cibersort_correlation_all <- as.data.frame(do.call(cbind, correlation_cibersort))
 cibersort_correlation_all$sample <- colnames(pseudo_bulk_data)
 cibersort_correlation_all <- melt(cibersort_correlation_all)
 cibersort_correlation_all_plot <- ggplot(cibersort_correlation_all, aes(x=variable, y=value)) + 
-  geom_boxplot()
+  geom_boxplot() + ylab("CIBERSORT Pearson correlation") +  xlab("noise iteration")
 
 
-save.image("~/Masterthesis/test2.RData")
+#save.image("~/Masterthesis/test2.RData")
 
 #######################################
 library(ggplot2)
@@ -218,7 +232,7 @@ library(ggpubr)
 # pearson_pval_comparison_plot <- ggplot(pearson_pval_comparison,
 #                                        aes(x = noise_iteration, y = pearson_pvalue, color = method)) + 
 #                                 geom_point()
-pearson_pval_comparison_plot
+#pearson_pval_comparison_plot
 # 
 # # compare rmsd of canceRdeconvolution and rmse of artdeco
 # rmsd_comparison <- data.frame(method = rep(c("SCDC", "bseqsc"), 
@@ -233,7 +247,7 @@ pearson_pval_comparison_plot
 # rmsd_comparison_plot <- ggplot(rmsd_comparison,
 #                                aes(x = noise_iteration, y = rmsd, color = method)) + 
 #   geom_point() # differences in each method are not visible anymore, plot them individually
-rmsd_comparison_plot
+#rmsd_comparison_plot
 # 
 # # compare pearson correlation of canceRdeconvolution and correlation of artdeco
 # corr_comparison <- data.frame(method = rep(c("SCDC", "bseqsc"), 
@@ -248,43 +262,47 @@ rmsd_comparison_plot
 # corr_comparison_plot <- ggplot(corr_comparison,
 #                                aes(x = noise_iteration, y = corr, color = method)) + 
 #   geom_point()
-corr_comparison_plot
+#corr_comparison_plot
 
 # compare scdc metrics of canceRdeconvolution and scdc metrics of artdeco
-metrics_comparison <- data.frame(method = rep(c("SCDC", "bseqsc"), 
-                                           each = length(error_cibersort)), 
-                                 noise_iteration = rep(error_cibersort, 2), 
-                                 rmsd = NA, mad = NA, R = NA)
-metrics_comparison$rmsd[1:length(error_cibersort)] <- 
-  sapply(scdc_metrics[error_cibersort], function(x) x[1])
-metrics_comparison$rmsd[(length(error_cibersort)+1):nrow(metrics_comparison)] <- 
-  sapply(scdc_metrics_cibersort[error_cibersort], function(x) x[1])
-metrics_comparison$mad[1:length(error_cibersort)] <- 
-  sapply(scdc_metrics[error_cibersort], function(x) x[2])
-metrics_comparison$mad[(length(error_cibersort)+1):nrow(metrics_comparison)] <- 
-  sapply(scdc_metrics_cibersort[error_cibersort], function(x) x[2])
-metrics_comparison$R[1:length(error_cibersort)] <- 
-  sapply(scdc_metrics[error_cibersort], function(x) x[3])
-metrics_comparison$R[(length(error_cibersort)+1):nrow(metrics_comparison)] <- 
-  sapply(scdc_metrics_cibersort[error_cibersort], function(x) x[3])
+# metrics_comparison <- data.frame(method = rep(c("SCDC", "bseqsc"), 
+#                                            each = length(error_cibersort)), 
+#                                  noise_iteration = rep(error_cibersort, 2), 
+#                                  rmsd = NA, mad = NA, R = NA)
+# metrics_comparison$rmsd[1:length(error_cibersort)] <- 
+#   sapply(scdc_metrics[error_cibersort], function(x) x[1])
+# metrics_comparison$rmsd[(length(error_cibersort)+1):nrow(metrics_comparison)] <- 
+#   sapply(scdc_metrics_cibersort[error_cibersort], function(x) x[1])
+# metrics_comparison$mad[1:length(error_cibersort)] <- 
+#   sapply(scdc_metrics[error_cibersort], function(x) x[2])
+# metrics_comparison$mad[(length(error_cibersort)+1):nrow(metrics_comparison)] <- 
+#   sapply(scdc_metrics_cibersort[error_cibersort], function(x) x[2])
+# metrics_comparison$R[1:length(error_cibersort)] <- 
+#   sapply(scdc_metrics[error_cibersort], function(x) x[3])
+# metrics_comparison$R[(length(error_cibersort)+1):nrow(metrics_comparison)] <- 
+#   sapply(scdc_metrics_cibersort[error_cibersort], function(x) x[3])
+# 
+# metrics_rmsd_comparison_plot <- ggplot(metrics_comparison,
+#                                        aes(x = noise_iteration, y = rmsd, color = method)) + 
+#   geom_point()
+# metrics_rmsd_comparison_plot
+# 
+# metrics_mad_comparison_plot <- ggplot(metrics_comparison,
+#                                       aes(x = noise_iteration, y = mad, color = method)) + 
+#   geom_point()
+# metrics_mad_comparison_plot
+# 
+# metrics_R_comparison_plot <- ggplot(metrics_comparison,
+#                                     aes(x = noise_iteration, y = R, color = method)) + 
+#   geom_point()
+# metrics_R_comparison_plot
 
-metrics_rmsd_comparison_plot <- ggplot(metrics_comparison,
-                                       aes(x = noise_iteration, y = rmsd, color = method)) + 
-  geom_point()
-metrics_rmsd_comparison_plot
 
-metrics_mad_comparison_plot <- ggplot(metrics_comparison,
-                                      aes(x = noise_iteration, y = mad, color = method)) + 
-  geom_point()
-metrics_mad_comparison_plot
-
-metrics_R_comparison_plot <- ggplot(metrics_comparison,
-                                    aes(x = noise_iteration, y = R, color = method)) + 
-  geom_point()
-metrics_R_comparison_plot
-
-
-ggarrange(spearman_pval_all_plot, cibersort_pearson_pval_all_plot, ncol = 1)
+ggarrange(pearson_pval_all_plot, spearman_pval_all_plot, 
+          #mad_pval_all_plot, 
+          #rmsd_pval_all_plot,
+          cibersort_pearson_pval_all_plot, 
+          ncol = 1)
 ggarrange(rmsd_scdc_plot, cibersort_rmse_all_plot, ncol = 1)
 ggarrange(pearson_scdc_plot, cibersort_correlation_all_plot, ncol = 1)
 
